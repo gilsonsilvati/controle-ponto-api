6478f0713c821e82732b1f29fd477acca4269ebe
Gilson Silva <gilsonsilvati@gmail.com>
2020-10-23 21:38:47 -0300

    Criando projeto

---
+++ src/test/java/com/controleponto/api/ControleDePontoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controleponto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
